b0f7b7b8daa9fff4f0dc9d7a51bd26bb5ed9a427
weffelms <stacie.weffelmeyer@wwt.com>
2020-01-30 11:09:47 -0600

    initial commit - setting up client and server

---
+++ client/src/components/ToDo.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <div>
+    <ul>
+      <li v-for='todo in todos' :key="todo">
+        <span>{{todo}}</span>
+      </li>
+    </ul>
+    <div>
+      {{ assignment }}
+    </div>
+  </div>
+</template>
+<script>
+import ToDoAPI from '@/services/ToDoAPI.js'
+export default {
+  data () {
+    return {
+      todos: [],
+      assignment: 'This is an assignment.',
+      assignments: ['assignment1', 'assignment2']
+    }
+  },
+  mounted () {
+    this.loadTodos()
+  },
+  methods: {
+    async loadTodos () {
+      const response = await ToDoAPI.getToDos()
+      this.todos = response.data
+    }
+  }
+}
+</script>
+<style lang="css">
+</style>
