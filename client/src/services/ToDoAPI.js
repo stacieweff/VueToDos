5d91e50fbafd89d5a743958a3aff6a396f018a0a
weffelms <stacie.weffelmeyer@wwt.com>
2020-02-08 14:44:40 -0600

    Connecting server to mongo db and starting to read from a collection

--- client/src/services/ToDoAPI.js
+++ client/src/services/ToDoAPI.js
@@ -2,6 +2,6 @@ import API from '@/services/API'
 
 export default {
   getToDos () {
-    return API().get('todo')
+    return API().get('todos')
   }
 }
--- server/src/app.js
+++ server/src/app.js
@@ -4,15 +4,48 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express() // create your express app// make app use dependencies
+const mongo = require('mongodb')
+const MongoClient = mongo.MongoClient
+const uri = 'mongodb+srv://databaseUser:databaseTestingPassword@cluster0-vlmbs.mongodb.net/todo-example?retryWrites=true&w=majority'
+
+let client
+let mongoClient = new MongoClient(uri, {
+  // reconnectTries: Number.MAX_VALUE,
+  // autoReconnect: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+mongoClient.connect((err, db) => {
+  if (err !== null) {
+    console.log(err)
+    return
+  }
+  client = db
+})
+
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cors())
-app.get('/todo', (req, res) => {
-  res.send([
-    'Thing 1',
-    'Thing 2',
-    'This is another thing!'
-  ])
+// app.get('/todo', (req, res) => {
+//   res.send([
+//     'Thing 1',
+//     'Thing 2',
+//     'This is another thing!'
+//   ])
+// })
+
+app.get('/todos', (req, res) => {
+  const collection = client.db('todo-example').collection('todos')
+  collection.find().toArray(function (err, results) {
+    if (err) {
+      console.log(err)
+      res.send([])
+      return
+    }
+
+    res.send(results)
+  })
 })
 
 app.listen(process.env.PORT || 8081) // client is already running on 8080
