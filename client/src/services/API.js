a9ccba9d2ed8edcf7239b301918297b54d9edd71
weffelms <stacie.weffelmeyer@wwt.com>
2020-02-20 20:00:53 -0600

    updating baseURL

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: `http://localhost:8081/` // the url of our server
+    baseURL: process.env.NODE_ENV === 'production' ? 'http://stacie-todos-api.herokuapp.com/' : `http://localhost:8081/` // the url of our server
   })
 }
